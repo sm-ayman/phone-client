f68624320dc2190d64dc7f9ad4675f85e01fac93
sm_ayman <sultanmdayman@gmail.com>
2025-10-27 16:09:05 +0600

    Phone Client Site has initiated and project setup done

---
+++ src/components/Phone.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Phone = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Phone;
---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
+import Phones from "./components/Phones.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/phones",
+        element: <Phones></Phones>,
+        loader: () => fetch("http://localhost:5000/phones"),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
